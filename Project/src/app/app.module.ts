99820a2d2bf79e8f2bfb34688e8994c12143da7f
Goli Anvitha <goli.anvitha@onmobile.com>
2023-01-06 16:08:43 +0530

    to mobile

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StocksSelectComponent } from './stocks-select/stocks-select.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocksSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Project/src/app/stocks-select/stocks-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stocks-select',
+  templateUrl: './stocks-select.component.html',
+  styleUrls: ['./stocks-select.component.css']
+})
+export class StocksSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
